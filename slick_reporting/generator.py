a67d19982706776df57baf4fa5a55417da5ddce5
lp55 <lp55@visum.pt>
2021-02-17 12:23:37 +0000

    Fixed identation

--- slick_reporting/generator.py
+++ slick_reporting/generator.py
@@ -58,14 +58,14 @@ class ReportGenerator(object):
        Same is true with __crosstab__ 
      """
      
-     hidden_columns = None
-     """
-     A list of columns that are hidden on the table view
+    hidden_columns = None
+    """
+    A list of columns that are hidden on the table view
      
-     This is useful when you need a column's data to format another, but don't need to show that to the user, or to merge column data into one
+    This is useful when you need a column's data to format another, but don't need to show that to the user, or to merge column data into one
      
-     Example: columns = ['product_id']
-     """
+    Example: columns = ['product_id']
+    """
 
     time_series_pattern = ''
     """
