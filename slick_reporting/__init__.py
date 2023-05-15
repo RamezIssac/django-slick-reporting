8659568064d46c156e90e88e06310eab8e8399ed
Ramez Ashraf <ramez@rasystem.io>
2023-05-15 10:48:51 +0300

    bump version

--- slick_reporting/__init__.py
+++ slick_reporting/__init__.py
@@ -1,5 +1,5 @@
-default_app_config = 'slick_reporting.apps.ReportAppConfig'
+default_app_config = "slick_reporting.apps.ReportAppConfig"
 
-VERSION = (0, 6, 8)
+VERSION = (0, 7, 0)
 
-__version__ = '0.6.8'
+__version__ = "0.7.8"
