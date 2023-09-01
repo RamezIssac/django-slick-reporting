a76f12395054c8193594b6ff9c65be79e2c0fdde
Ramez Ashraf <ramez@rasystems.io>
2023-09-01 09:15:56 +0300

    * Add slick reporting to path

--- demo_proj/demo_proj/wsgi.py
+++ demo_proj/demo_proj/wsgi.py
@@ -7,10 +7,12 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "demo_proj.settings")
+BASE_DIR = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "../")
+sys.path.append(os.path.abspath(BASE_DIR))
 
 application = get_wsgi_application()
