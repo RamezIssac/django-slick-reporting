8e7ab544368fa8d5308012c0fc67762cd81fb77b
Ramez Ashraf <ramez@rasystems.io>
2023-09-01 09:42:51 +0300

    * Configure live settings for wsgi

--- demo_proj/demo_proj/wsgi.py
+++ demo_proj/demo_proj/wsgi.py
@@ -11,7 +11,7 @@ import os, sys
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "demo_proj.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "demo_proj.settings_production")
 BASE_DIR = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "../")
 sys.path.append(os.path.abspath(BASE_DIR))
 
